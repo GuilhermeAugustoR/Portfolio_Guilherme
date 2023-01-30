e6cd4008935bcf1845f7976ad728a4ad0c000830
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2023-01-30 16:57:55 -0300

    ✨ Create Portfolio

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import "./styles.module.css"
+import React from "react";
+import { Img } from "react-image";
+import "./styles.css";
+import { porfolios } from "./portfolios";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio_container">
+        {porfolios.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <Img alt={title} src={image} />
+              </div>
+
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/Portfolio/portfolios.tsx
@@ -0,0 +1,26 @@
+export const porfolios = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
+    title: "This is a portfolio item title 2",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
+    title: "This is a portfolio item title 3",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+];
