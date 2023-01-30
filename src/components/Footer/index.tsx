78638a630edc70cadebe8aa1d70b3516fc054cb3
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2023-01-30 19:16:40 -0300

    ✨ Create Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import "./styles.module.css";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
+import "./styles.css";
 
 const Footer = () => {
-  return <section id="portfolio">Footer</section>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        GARC
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="http://linkedin.com" target="_blank" rel="noreferrer">
+          <AiOutlineLinkedin />
+        </a>
+        <a href="http://github.com" target="_blank" rel="noreferrer">
+          <AiOutlineGithub />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; GARC. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
