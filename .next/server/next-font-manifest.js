2ebd8f9bf39fdb528f3a2e9cc36a86f857a6cd80
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2025-08-17 17:54:24 -0300

    🔥

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/guilhermeaugusto/Desktop/Projects/MeusProjetos/Portfolio_Guilherme/app/layout\":[\"static/media/5b01f339abf2f1a5.p.woff2\",\"static/media/028c0d39d2e8f589-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
